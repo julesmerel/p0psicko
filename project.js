72991178ebe6e630ec3f9e086ea976e9425cba5b
julesmerel <168073532+julesmerel@users.noreply.github.com>
2024-04-25 20:01:14 -0600

    Add files via upload

---
+++ project.js
@@ -0,0 +1,43 @@
+let mynaranja=document.getElementsByTagName('button_naranja');
+function topNaranja(){
+  document.body.scrollTop=0;
+  document.documentElement.scrollTop=0;
+}
+
+let mynegro=document.getElementsByTagName('button_Negro');
+function topNegro(){
+  document.body.scrollTop=0;
+  document.documentElement.scrollTop=0;
+}
+
+let contact=document.getElementsByTagName('shortcut_Contacto');
+function topContacto(){
+  document.body.scrollTop=2100;
+  document.documentElement.scrollTop=2100;
+}
+
+let trabajo=document.getElementsByTagName('shortcut_Trabajo');
+function topTrabajo(){
+  document.body.scrollTop=687;
+  document.documentElement.scrollTop=687;
+}
+
+let inicio=document.getElementsByTagName('shortcut_Inicio');
+function topInicio(){
+  document.body.scrollTop=-40;
+  document.documentElement.scrollTop=-40;
+}
+
+let go_insta=document.getElementsByTagName('instagram_url');
+function ig(){
+  window.location.href = "https://www.instagram.com/p0psicko?igsh=YzYzajM0dDVyYmV6";
+}
+
+let go_twt=document.getElementsByTagName('twitter_url');
+function twt(){
+  window.location.href = "https://x.com/p0psicko_?t=n8vD8cJNBAuCsyBwHL7UXg&s=09";
+}
+let go_error=document.getElementsByTagName('error_');
+function err0r(){
+  window.location.href = "https://julesmerel.github.io/error404/";
+}
